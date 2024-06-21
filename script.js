c8017d810965519df42123037ce743bdcb31405a
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-06-21 10:58:22 -0300

    Adicionando janela sobreposta de menu

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('menu').style.display = 'block';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    this.style.display = 'none';
+    document.getElementById('menu').style.display = 'none';
+});
+
+document.querySelectorAll('.menuLink').forEach(link => {
+    link.addEventListener('click', function() {
+        document.getElementById('overlay').style.display = 'none';
+        document.getElementById('menu').style.display = 'none';
+    });
+});
