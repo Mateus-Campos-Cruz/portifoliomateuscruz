1b3d85f3e0061c60774866b0f6b144a0b8dc1a99
mateus-campos-cruz <mateusfelipecamposdacruz@gmail.com>
2024-06-24 11:57:31 -0300

    Adicionando menu barra, faltando css

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById('menu').style.display = 'block';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu-icon').addEventListener('click', function() {
+        document.getElementById('overlay').style.display = 'block';
+        document.getElementById('menu').style.display = 'block';
+    });
 
-document.getElementById('overlay').addEventListener('click', function() {
-    this.style.display = 'none';
-    document.getElementById('menu').style.display = 'none';
-});
-
-document.querySelectorAll('.menuLink').forEach(link => {
-    link.addEventListener('click', function() {
-        document.getElementById('overlay').style.display = 'none';
+    document.getElementById('overlay').addEventListener('click', function() {
+        this.style.display = 'none';
         document.getElementById('menu').style.display = 'none';
+    });
+
+    document.querySelectorAll('.menuLink').forEach(link => {
+        link.addEventListener('click', function() {
+            document.getElementById('overlay').style.display = 'none';
+            document.getElementById('menu').style.display = 'none';
+        });
     });
 });
